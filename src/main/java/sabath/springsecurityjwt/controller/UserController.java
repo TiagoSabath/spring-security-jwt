12751cff637e2271aa9b1266cb8c65b106afa35a
Tiago Sabath <tiago.sabath@gmail.com>
2023-12-04 19:01:25 -0300

    Initial commit

---
+++ src/main/java/sabath/springsecurityjwt/controller/UserController.java
@@ -0,0 +1,20 @@
+package sabath.springsecurityjwt.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sabath.springsecurityjwt.model.User;
+import sabath.springsecurityjwt.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        service.createUser(user);
+    }
+}
---
+++ src/main/java/sabath/springsecurityjwt/service/UserService.java
@@ -0,0 +1,23 @@
+package sabath.springsecurityjwt.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import sabath.springsecurityjwt.model.User;
+import sabath.springsecurityjwt.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
+
